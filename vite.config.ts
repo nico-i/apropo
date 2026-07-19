77137f08a051cd36c8f4341d49be6ca857fe28eb
Nico Ismaili <nico.ismaili@aoe.com>
2026-07-19 18:19:07 +0200

    fix: paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/apropo/' : '/',
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
